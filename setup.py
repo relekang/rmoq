714ac8a8128054cdedcc3822ad131582b6528756
Rolf Erik Lekang <me@rolflekang.com>
2014-12-30 09:23:50 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='rmoq',
+    version='0.0.1',
+    url='http://github.com/relekang/rmoq',
+    author='Rolf Erik Lekang',
+    author_email='me@rolflekang.com',
+    description='A simple request-mocker that will download',
+    py_modules=['rmoq'],
+    license='MIT',
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
