11a5a4ce29b9e85faee44d8f69930b5f1493642f
Rolf Erik Lekang <me@rolflekang.com>
2014-12-30 09:29:12 +0100

    Add rmoq decorator

---
+++ rmoq.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import six
+import requests
+
+from requests.packages.urllib3 import HTTPResponse
+
+if six.PY3:
+    from unittest import mock
+    from io import BytesIO as BufferIO
+else:
+    import mock
+    from six import StringIO as BufferIO
+
+
+class Mock(object):
+    path = 'fixtures'
+
+    def __enter__(self):
+        def on_send(session, request, *args, **kwargs):
+            return self.on_request(session, request, *args, **kwargs)
+
+        self.patch = mock.patch('requests.Session.send', on_send)
+        self.patch.start()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.patch.stop()
+
+    def activate(self, path=None):
+        if path is not None:
+            self.path = path
+
+        def activate(func):
+            def wrapper(*args, **kwargs):
+                with self:
+                    func(*args, **kwargs)
+
+            return wrapper
+
+        return activate
+
+    def on_request(self, session, request, *args, **kwargs):
+        response_path = os.path.join(os.getcwd(), self.path, self._get_filename(request.url))
+
+        if os.path.exists(response_path):
+            content_type, content = self._read_body_from_file(response_path)
+            response = HTTPResponse(
+                status=200,
+                body=BufferIO(content),
+                preload_content=False,
+                headers={'Content-Type': content_type}
+            )
+            adapter = session.get_adapter(request.url)
+            response = adapter.build_response(request, response)
+        else:
+            self.patch.stop()
+            response = requests.get(request.url)
+            self.patch.start()
+            self._write_body_to_file(response_path, response.text, response.headers['Content-Type'])
+
+        return response
+
+    @staticmethod
+    def _get_filename(url):
+        return re.sub(r'/$', '', re.sub(r'https?://', '', url)).replace('/', '_')
+
+    @staticmethod
+    def _read_body_from_file(path):
+        with open(path) as f:
+            content = f.read()
+            content_type = content.split('\n')[0]
+            content = '\n'.join(content.split('\n')[1:])
+            return content_type, content.encode('utf-8')
+
+
+    @staticmethod
+    def _write_body_to_file(path, content, content_type):
+        directory = os.path.dirname(path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        with open(path, 'w') as f:
+            f.write('{}\n'.format(content_type))
+            f.write(content)
+
+
+_mock = Mock()
+
+__all__ = []
+for __attr in (a for a in dir(_mock)):
+    __all__.append(__attr)
+    globals()[__attr] = getattr(_mock, __attr)
---
+++ test_rmoq.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import os
+import shutil
+import unittest
+import requests
+from datetime import datetime
+
+import rmoq
+
+
+def timer(func, *args):
+    start_time = datetime.now()
+    result = func(*args)
+    end_time = datetime.now()
+    return end_time - start_time, result
+
+
+class MockTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        directory = os.path.join(os.getcwd(), 'fixtures')
+        if os.path.exists(directory):
+            shutil.rmtree(directory)
+
+    def assert_response(self, response, status_code=200):
+        self.assertEqual(response.status_code, status_code)
+        self.assertGreater(len(response.text), 100)
+        self.assertEqual(response.headers['content-type'], 'text/html; charset=utf-8')
+
+    def test_rmoq_decorator(self):
+
+        @rmoq.activate()
+        def perform_requests():
+            first_timedelta, first_response = timer(requests.get, 'http://rolflekang.com')
+            self.assertTrue(os.path.exists(os.path.join(os.getcwd(), 'fixtures/rolflekang.com')))
+            last_timedelta, last_response = timer(requests.get, 'http://rolflekang.com')
+            self.assertGreater(first_timedelta, last_timedelta)
+            self.assert_response(first_response)
+            self.assert_response(last_response)
+
+        perform_requests()
+        self.assertTrue(os.path.exists(os.path.join(os.getcwd(), 'fixtures/rolflekang.com')))
+
+    def test_rmoq_decorator_with_path(self):
+        @rmoq.activate('path')
+        def perform_requests():
+            requests.get('http://rolflekang.com/feed.xml')
+
+        perform_requests()
+        self.assertTrue(os.path.exists(os.path.join(os.getcwd(), 'path/rolflekang.com')))
+
+    def test_get_filename(self):
+        self.assertEqual(rmoq.Mock._get_filename('http://rolflekang.com'), 'rolflekang.com')
+        self.assertEqual(rmoq.Mock._get_filename('http://rolflekang.com/'), 'rolflekang.com')
+        self.assertEqual(rmoq.Mock._get_filename('http://rolflekang.com/feed.xml'), 'rolflekang.com_feed.xml')
+
