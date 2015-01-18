9e2f4b3cadeaa6857ee284f0131d59c85bb6245f
Rolf Erik Lekang <me@rolflekang.com>
2015-01-18 15:27:17 +0100

    0.2.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(*parts):
 
 setup(
     name='rmoq',
-    version='0.2.4',
+    version='0.2.5',
     url='http://github.com/relekang/rmoq',
     author='Rolf Erik Lekang',
     author_email='me@rolflekang.com',
