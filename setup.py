dde42f95dcd0831fddf18c86501466588076916d
requires.io <support@requires.io>
2015-08-02 20:51:38 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import re
+import sys
 import codecs
 from os import path
 
@@ -8,9 +10,22 @@ def read(*parts):
     file_path = path.join(path.dirname(__file__), *parts)
     return codecs.open(file_path, encoding='utf-8').read()
 
+with open('rmoq/__init__.py', 'r') as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(),
+        re.MULTILINE
+    ).group(1)
+
+try:
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError:
+    pass
+
 setup(
     name='rmoq',
-    version='0.4.1',
+    version=version,
     url='http://github.com/relekang/rmoq',
     author='Rolf Erik Lekang',
     author_email='me@rolflekang.com',
@@ -18,7 +33,7 @@ setup(
     long_description=read('README.rst'),
     packages=find_packages(exclude='tests'),
     install_requires=[
-        'mock==1.1.0',
+        'mock==1.3.0',
         'six==1.9.0',
         'requests',
     ],
